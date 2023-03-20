7333070167a49fb89ed284576433cb6715755202
45an <114033186+45an@users.noreply.github.com>
2023-03-20 02:03:14 +0100

    Fix the code more readable

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Configuration;
+using System.Configuration;
 using Dapper;
 using Npgsql;
 using System.Data;
@@ -14,56 +14,58 @@ class Program
 
     static void Menysystem()
     {
+
+        Console.WriteLine("Welcome to The Wealthy Inc. Time reporting system!");
+
+        bool showMenySystem = true;
+
+        while (showMenySystem)
         {
-            Console.WriteLine("Welcome to the time reporting system!");
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1. Add person");
+            Console.WriteLine("2. Add project");
+            Console.WriteLine("3. Register time worked on project");
+            Console.WriteLine("4. Exit");
+            Console.Write("===> ");
 
-            while (true)
+            string? input = Console.ReadLine();
+
+            switch (input)
             {
-                Console.WriteLine("Please select an option:");
-                Console.WriteLine("1. Add person");
-                Console.WriteLine("2. Add project");
-                Console.WriteLine("3. Register time worked on project");
-                Console.WriteLine("4. Exit");
-                Console.Write("===> ");
-
-                string? input = Console.ReadLine();
-
-                switch (input)
-                {
-                    case "1":
-                        AddPerson();
-                        Console.WriteLine();
-                        break;
-                    case "2":
-                        AddProject();
-                        Console.WriteLine();
-                        break;
-                    case "3":
-                        RegisterTime();
-                        Console.WriteLine();
-                        break;
-                    case "4":
-                        Console.WriteLine("Exiting program...");
-                        return;
-                    default:
-                        Console.WriteLine("Invalid input. Please try again.");
-                        break;
-                }
+                case "1":
+                    AddPerson();
+                    Console.WriteLine();
+                    break;
+                case "2":
+                    AddProject();
+                    Console.WriteLine();
+                    break;
+                case "3":
+                    RegisterTime();
+                    Console.WriteLine();
+                    break;
+                case "4":
+                    Console.WriteLine("Exiting program...");
+                    return;
+                default:
+                    Console.WriteLine("Invalid input. Please try again.");
+                    break;
             }
         }
+    }   
 
-        static void AddPerson()
-        {
+    static void AddPerson()
+    {
             Console.WriteLine("Enter name of person:");
             string name = Console.ReadLine();
 
             // Call method in PostgresqlConnection class to add person to database
             PostgresqlConnection.CreateUser(name);
             Console.WriteLine($"{name} added successfully!");
-        }
+    }
 
-        static void AddProject()
-        {
+    static void AddProject()
+    {
             Console.WriteLine("Enter name of project:");
             string addProjectName = Console.ReadLine();
 
@@ -71,10 +73,10 @@ class Program
             PostgresqlConnection.CreateProject(addProjectName);
             Console.WriteLine($"Your new projectname is {addProjectName}");
             Console.WriteLine($"{addProjectName} project added successfully!");
-        }
+    }
 
-        static void RegisterTime()
-        {
+    static void RegisterTime()
+    {
             Console.WriteLine("Enter name of person:");
             string personName = Console.ReadLine();
 
@@ -92,13 +94,7 @@ class Program
             PostgresqlConnection.TimeReport(projectNames, personNames, hoursWorked);
             Console.WriteLine($"Hi {personName}, You have been working in this project {projectName} in the last {hoursWorked} hours");
             Console.WriteLine("Time registered successfully!");
-        }
-
-        
-
-
     }
-
+    
 }
-
 
